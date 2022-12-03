67dcc0edaf6af40388ae2ef12b015dc112c3d8ae
ARandomUser123 <eric.zhusc@gmail.com>
2022-12-03 11:07:05 +0800

    implemented basic features

---
+++ pitch-detector/src/components/Navbar.js
@@ -0,0 +1,28 @@
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+
+export default function MyNav() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container fluid>
+        <Navbar.Brand href="./">Eric's Pitch Detector</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+          </Nav>
+          <Navbar.Brand href="https://github.com/ARandomUser123">
+            <img
+              src="./github.png"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+              alt="React Bootstrap logo"
+            />
+          </Navbar.Brand>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
