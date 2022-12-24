e5291d5f13aa3981edc5b800b2745c669d04e504
ARandomUser123 <eric.zhusc@gmail.com>
2022-12-24 11:01:47 +0800

    added file detection

--- pitch-detector/src/components/Main.js
+++ pitch-detector/src/components/Main.js
@@ -6,12 +6,14 @@ import Button from "react-bootstrap/Button";
 import { PitchDetector } from "pitchy";
 import { useState, useRef } from "react";
 import pitchMap from "../utils/pitchMap";
+import Form from "react-bootstrap/Form";
 
 export default function Main() {
   const audio = useRef(null);
   const [timer, setTimer] = useState(null);
   const [pitch, setPitch] = useState(null);
   const [history, setHistory] = useState([]);
+  const [file, setFile] = useState(null);
   const released = useRef(true);
 
   const clearHistory = () => {
@@ -43,6 +45,7 @@ export default function Main() {
     const detector = PitchDetector.forFloat32Array(analyser.fftSize);
     const input = new Float32Array(detector.inputLength);
     analyser.getFloatTimeDomainData(input);
+    console.log(input);
     const [frequency, clarity] = detector.findPitch(
       input,
       audioContext.sampleRate
@@ -78,33 +81,91 @@ export default function Main() {
   return (
     <Container style={{ marginTop: 30 }}>
       <Row style={{ marginBottom: 30 }}>
+        {`Microphone stream ${audio.current == null ? "not " : ""}ready!`}
+      </Row>
+      <Row style={{ marginBottom: 30 }}>
         <Col>
           <Button
             style={{ width: "100%" }}
             onClick={audio.current == null ? startDetection : stopMicrophone}
           >
-            {audio.current == null ? "Start Detection" : "Stop microphone"}
+            {audio.current == null ? "Start microphone" : "Stop microphone"}
           </Button>
         </Col>
         <Col>
           <Button style={{ width: "100%" }} onClick={clearHistory}>
-            Reset History
+            Reset history
           </Button>
         </Col>
       </Row>
-      <Row style={{ margiTop: 30 }} className="justify-content-center">
+
+      <Row
+        style={{ marginTop: 30, marginBottom: 30 }}
+        className="justify-content-center"
+      >
         <Card style={{ width: "18rem" }}>
           <Card.Body>
             <Card.Title></Card.Title>
-            <Card.Text>
-              <h2>{pitch}</h2>
-            </Card.Text>
+            <Card.Text style={{ fontSize: 24 }}>{pitch}</Card.Text>
             {/* <Button variant="primary">Go somewhere</Button> */}
           </Card.Body>
         </Card>
       </Row>
-      <Row> {audio.current == null ? "Audio not ready!" : "Audio ready"} </Row>
+
       <Row> {history.join(",")} </Row>
+      <Row style={{ marginBottom: 30 }}>
+        <Form.Control
+          type="file"
+          onChange={(event) => {
+            const files = event.target.files;
+            if (files.length > 0) {
+              const audioFile = files[0];
+              setFile(audioFile);
+            }
+          }}
+        />
+      </Row>
+      <Row>
+        <Button
+          variant="primary"
+          onClick={() => {
+            const reader = new FileReader();
+            reader.readAsArrayBuffer(file);
+            reader.onload = async () => {
+              const arrayBuffer = reader.result;
+              // const float32Array = Float32Array.from(arrayBuffer);
+              const offlineAudioContext = new OfflineAudioContext({
+                length: 1,
+                sampleRate: 44100,
+              });
+              const audioBuffer = await offlineAudioContext.decodeAudioData(
+                arrayBuffer
+              );
+              for (
+                let i = 0;
+                i < audioBuffer.length;
+                i += audioBuffer.sampleRate / 10
+              ) {
+                const input = new Float32Array(audioBuffer.sampleRate / 10);
+                audioBuffer.copyFromChannel(input, 0, i);
+                const detector = PitchDetector.forFloat32Array(
+                  audioBuffer.sampleRate / 10
+                );
+                // console.log(input);
+                const [frequency, clarity] = detector.findPitch(
+                  input,
+                  offlineAudioContext.sampleRate
+                );
+                // console.log(frequency, clarity);
+                const pitchNew = freqToPitch(frequency);
+                console.log("pitchNew", pitchNew);
+              }
+            };
+          }}
+        >
+          Start file detection
+        </Button>
+      </Row>
     </Container>
   );
 }
